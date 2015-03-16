a50985823bf75589a361d9c4d893fe81712dc936
Tom Mertz & Conor Baumgart <student@epicodus.com>
2015-03-16 16:31:44 -0700

    did some stuff

--- app/app.php
+++ app/app.php
@@ -1,10 +1,11 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
 
     $app = new Silex\Application();
 
-    $DB = new PDO('psql:host=localhost;dbname=to_do');
+    $DB = new PDO('pgsql:host=localhost;dbname=to_do');
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array (
         'twig.path' => __DIR__.'/../views'
@@ -12,19 +13,39 @@
 
     $app->get("/", function() use ($app) {
 
-        return $app['twig']->render('tasks.php', array('tasks' => Task::getAll()));
+        return $app['twig']->render('index.twig');
 
     });
-        $app->post("/tasks", function() use ($app) {
-            $task = new Task($_POST['description']);
-            $task->save();
-            return $app['twig']->render('create_task.php', array('newtask' => $task));
+
+    $app->get("/tasks", function() use ($app) {
+        return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
     });
 
-        $app->post("/delete_tasks", function() use ($app) {
-            Task::deleteAll();
-            return $app['twig']->render('delete_tasks.php');
-        });
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
+    });
 
-        return $app;
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.twig');
+    });
+
+    $app->post("/tasks", function() use ($app) {
+        $task = new Task($_POST['description']);
+        $task->save();
+        return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->post("/delete_tasks", function() use ($app) {
+        Task::deleteAll();
+        return $app['twig']->render('index.twig');
+    });
+
+    return $app;
 ?>
