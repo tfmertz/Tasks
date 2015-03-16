1eb8015e721d68f5a34bf0ebb6f1d07e574fca4f
Tom Mertz & Conor Baumgart <student@epicodus.com>
2015-03-16 10:55:52 -0700

    Add id tests

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -12,6 +12,11 @@
     class TaskTest extends PHPUnit_Framework_TestCase
     {
 
+        protected function tearDown()
+        {
+            Task::deleteAll();
+        }
+
         function test_save()
         {
             $description = "Wash the dog";
@@ -38,6 +43,53 @@
 
             //assert
             $this->assertEquals([$test_Task, $test_Task2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $description2 = "Water the lawn";
+            $test_Task = new Task($description);
+            $test_Task->save();
+            $test_Task2 = new Task($description2);
+            $test_Task2->save();
+
+            //Act
+            Task::deleteAll();
+
+            //Assert
+            $result = Task::getAll();
+            $this->assertEquals([], $result);
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $id = 1;
+            $test_Task = new Task($decription, $id);
+
+            //Act
+            $result = $test_Task->getId();
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
+        function test_setId()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $id = null;
+            $test_Task = new Task($description, $id);
+
+            //Act
+            $test_Task->setId(2);
+
+            //Assert
+            $result = $test_Task->getId();
+            $this->assertEquals(2, $result);
         }
     }
 ?>
